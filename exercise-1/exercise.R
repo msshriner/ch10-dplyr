5d4fc9bf0a0df55c70214429bc6e2705ae738577
msshriner <markusshriner@gmail.com>
2018-04-11 16:53:34 -0700

    participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -11,18 +11,18 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
+View(vehicles)
 
-
-# Select the different manufacturers (makes) of the cars in this data set. 
+# Select the different manufacturers (makes) of the cars in this data set.
 # Save this vector in a variable
-
+makes <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+unique(makes)
 
 # Filter the data set for vehicles manufactured in 1997
-
+# filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
@@ -32,20 +32,20 @@ library(fueleconomy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+cars_1997$average = (cars_1997$hwy + cars_1997$cty) / 2
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
-# than 20 miles/gallon in the city. 
+# than 20 miles/gallon in the city.
 # Save this new data frame in a variable.
 
 
-# Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
+# Of the above vehicles, what is the vehicle ID of the vehicle with the worst
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
+worst_hwy <- two_wheel_20_mpg[two_wheel_20$hwy]
 
-
-# Write a function that takes a `year_choice` and a `make_choice` as parameters, 
-# and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
+# Write a function that takes a `year_choice` and a `make_choice` as parameters,
+# and returns the vehicle model that gets the most hwy miles/gallon of vehicles
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
 
